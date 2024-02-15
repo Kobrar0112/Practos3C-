70eb9c81ff7b288c73c36a3cb38abe696062d8b6
Kobrar0112 <85014618+Kobrar0112@users.noreply.github.com>
2024-02-15 11:30:14 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int a, b;
+
+    cout << "Введите количество строк массива: ";
+    cin >> a;
+
+    cout << "Введите количество столбцов массива: ";
+    cin >> b;
+
+    // Выделение памяти для двумерного массива
+    double** arr = new double* [a];
+    for (int i = 0; i < a; i++) {
+        arr[i] = new double[b];
+    }
+
+    cout << "Введите элементы двумерного массива (" << a << "x" << b << "):" << endl;
+
+    // Ввод элементов массива
+    for (int i = 0; i < a; i++) {
+        double sum = 0;
+        for (int j = 0; j < b; j++) {
+            cin >> arr[i][j];
+            sum += arr[i][j];
+        }
+        double ab = sum / b;
+        cout << "Среднее арифметическое для строки " << i + 1 << ": " << ab << endl;
+    }
+
+   
+    return 0;
+}
